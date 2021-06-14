8630b717d098ea464c8837bdae118d1d8f081482
Carlos A. Selman D <cselman-2018325@kinal.edu.gt>
2021-06-13 21:22:57 -0600

    Creación del user.rutes.js

--- src/rutes/user.rutes.js
+++ src/rutes/user.rutes.js
@@ -1,12 +1,23 @@
+////////////////////////Selman//////////////////////////////////////////
 'use strict'
 
+// IMPORTS
 const express = require("express");
-var authenticated = require("../middlewares/authenticated");
 const userController = require("../controllers/user.controller");
 
+// MIDDLEWARES
+var authenticated = require("../middlewares/authenticated");
+
+// RUTES
 var api = express.Router();
 
-
-
+    api.post('/Login', userController.Login);
+    api.post('/createUser', userController.createUser);
+    api.delete('/dropUser', authenticated.ensureAuth, userController.dropUser);
+    api.put('/editUser', authenticated.ensureAuth, userController.editUser);
+    api.get('/showUserId/:idUsuario', userController.findUserId)
+    api.get('/showAllUser', authenticated.ensureAuth, userController.showAllUsers)
 
 module.exports = api;
+
+
