22b249043520332583d07a607d3c1fa675f82ea6
Carlos A. Selman D <cselman-2018325@kinal.edu.gt>
2021-06-13 21:19:58 -0600

    Culminación del app.js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-a
+'use strict'
+
+//VARIABLES GLOBALES
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser")
+const cors = require("cors")
+
+// IMPORTACION RUTAS
+const user_controller = require("./src/controllers/user.controller");
+const user_rutes = require("./src/rutes/user.rutes");
+
+// MIDDLEWARES
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+
+// CABECERAS
+app.use(cors());
+
+// CARGA DE RUTAS
+app.use('/api', user_rutes);
+
+
+user_controller.createUserStaticAdmin();
+//EXPORTAR
+module.exports = app;
