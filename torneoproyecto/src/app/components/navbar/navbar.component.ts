3dc9eef3d4135a4b323b2d4d70e3f65e8b6d5ab0
Carlos A. Selman D <cselman-2018325@kinal.edu.gt>
2021-06-14 18:38:55 -0600

    Actualización y cambios en el navbar, con nuevas funciones

--- torneoproyecto/src/app/components/navbar/navbar.component.ts
+++ torneoproyecto/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,80 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  providers: [UserService]
 })
 export class NavbarComponent implements OnInit {
+  public userModel: User;
+  public token;
+  public identidad;
 
-  constructor() { }
+
+  constructor(public userService:UserService,
+    private router:Router) {
+      this.identidad = this.userService.getIdentidad();
+      this.userModel = new User("","","","","","","","");
+
+   }
+
 
   ngOnInit(): void {
   }
 
+  login(){
+
+    this.userService.login(this.userModel).subscribe(
+      response=>{
+        console.log(response)
+        this.refresh()
+        this.identidad = response.userFound
+        localStorage.setItem('identidad', JSON.stringify(this.identidad))
+        this.getToken();
+        this.token=response.token;
+        localStorage.setItem('token', JSON.stringify(this.token));
+        this.router.navigate(['/inicio']);
+      },
+      error=>{
+        console.log(<any>error);
+        Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: 'Email o contraseña incorrecto',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+    )
+  }
+
+  getToken(){
+    this.userService.login(this.userModel).subscribe(
+      response=>{
+        console.log(response)
+        this.token=response.token;
+        localStorage.setItem('token', JSON.stringify(this.token));
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: 'Email o contraseña incorrecto',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+    )
+  }
+
+  navegarCuenta(idUsuario){
+    this.router.navigate(['/cuenta', idUsuario])
+  }
+
+  refresh(): void{
+    window.location.reload();
+  }
 }
