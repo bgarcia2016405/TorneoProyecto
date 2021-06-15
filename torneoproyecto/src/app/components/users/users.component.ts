caa5e52605bbfaf22344ca73e53f3383969b0de4
Carlos A. Selman D <cselman-2018325@kinal.edu.gt>
2021-06-14 18:32:28 -0600

    Actualización con mejoras en el componente de user

--- torneoproyecto/src/app/components/users/users.component.ts
+++ torneoproyecto/src/app/components/users/users.component.ts
@@ -1,15 +1,35 @@
+
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { UserService } from "../../services/user.service";
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
+  providers: [UserService]
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  public usuarios;
+  public idUsuarioModel: User;
+  constructor(public userService: UserService) {
+    this.idUsuarioModel = new User("","","","","","","","");
+   }
 
   ngOnInit(): void {
+    this.obtenerUsuarios();
   }
 
+  obtenerUsuarios(){
+    this.userService.showAllUser().subscribe(
+      response => {
+        this.usuarios = response
+        console.log(response)
+      }
+    )
+  }
+
+
+
 }
+
